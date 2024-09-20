31dd40c389c3804d6c12ce5702d1a323a3154c85
Alexandr Gromov <gromovaleksandr813@gmail.com>
2024-09-20 23:29:48 +0300

    add home_work_2_1_2

---
+++ home_work_2_1_2/home_work_2_1_2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+struct bank_account
+{
+        int account_number;
+        std::string owner_name;
+        double balance;
+};
+double changing_balance(bank_account* p, double n) {
+    p->balance = n;
+    return 0;
+};
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    bank_account client;
+    double new_balance;
+    std::cout << "¬ведите номер счЄта : ";
+    std::cin >> client.account_number;
+    std::cout << "¬ведите им€ владельца: ";
+    std::cin >> client.owner_name;
+    std::cout << "¬ведите баланс: ";
+    std::cin >> client.balance;
+    std::cout << "¬ведите новый баланс: ";
+    std::cin >> new_balance;
+    changing_balance(&client, new_balance);
+    std::cout << "¬аш счЄт: " << client.owner_name << ", "
+    << client.account_number << ", " << int(client.balance) << "."
+    << int((client.balance - int(client.balance))*100) << std::endl;
+    return EXIT_SUCCESS;
+}
+
